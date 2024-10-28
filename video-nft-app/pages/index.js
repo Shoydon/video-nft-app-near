45a14c94935e79215beb3a896831a06dc9ce762f
Shoydon <shoydon@gmail.com>
2024-10-28 16:57:49 +0530

    deposit amount code

--- video-nft-app/pages/index.js
+++ video-nft-app/pages/index.js
@@ -84,7 +84,7 @@ export default function IndexPage() {
   const mintNFTs = async (uri, price) => {
     if (!signedAccountId) return;
     try {
-      const depositAmount = BigInt(price * 1000000000000000000000000n);
+      const depositAmount = BigInt(price) * BigInt(1000000000000000000000000n);
       console.log(depositAmount);
       const tx = await wallet.callMethod({
         contractId: CONTRACT,
